61d6e0fe37025a4370cee8908631f81ed621bf98
Samuel Archibald <samuel.archibald00@gmail.com>
2020-08-29 00:52:28 -0400

    Changed coloring

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,13 @@ pub struct App {
     values: [[f32; 8]; 8],
     last_updated: f64,
     line: i32,
+    max_value: f32,
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs, values: [[f32; 8]; 8]) {
         use graphics::*;
-
+        let M = self.max_value;
         self.gl.draw(args.viewport(), |c, gl| {
             const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
             // Clear the screen.
@@ -41,8 +42,14 @@ impl App {
                         (args.window_size[0] / 8.0) * x,
                         (args.window_size[1] / 8.0) * y,
                     );
+                    
+                    let mut color = colorFromValue(values[value_y][value_x], 0.0, M);
 
-                    let color = [values[value_y][value_x], 0.0, 0.0, 1.0];
+                    // color[0] = 1.0;
+                    // color[1] = 0.0;
+                    // color[2] = 0.0;
+
+                    println!("Color: {:#?}", color);
                     let s = [0.0, 0.0, args.window_size[0] / 8.0, args.window_size[1] / 8.0];
                     rectangle(color, s, trans, gl);
                     x += 1.0;
@@ -55,23 +62,46 @@ impl App {
     fn update(&mut self, args: &UpdateArgs, filename: &String, values: &mut [[f32; 8]; 8]) {
         self.last_updated += args.dt;
         
-        if self.last_updated >= 0.5{
+        if self.last_updated >= 0.2{
             self.last_updated = 0.0;
             let (cv, r) = read(filename, self.line);
             if !r{
                 println!("Ran {} frames", self.line);
                 std::process::exit(0);
-                return;
+                // return;
             }
             self.line += 1;
             
             for y in 0..8{
                 for x in 0..8{
+                    //cv[y][x] = map(cv[y][x], range_min, range_max, 0.0, 1.0);
+                    //println!("{}", values[y][x]);
                     values[y][x] = cv[y][x];
+                    //println!("{}", values[y][x]);
                 }
             }
         }
     }
+}
+
+fn colorFromValue(val: f32, min: f32, max: f32) -> [f32;4] {
+    let mut result = [0.0;4];
+    result[3] = 1.0;
+    println!("{:#?}",max);
+    // result[0] = ((val-min) / (max-min) * 250.0 + (val-min)/(max-min) * 300.0) / 100.0;
+    // result[1] = ((val-min) / (max-min) * 150.0 + (val-min)/(max-min) * 200.0) / 100.0;
+    // result[2] = ((val-min) / (max-min) * 0.0 + (val-min)/(max-min) * 50.0) / 100.0;
+
+    result[0] = map(val, 0.0, max, 0.0, 1.0);
+    result[1] = map(val, 0.0, max, 0.0, 1.0);
+    result[2] = map(val, 0.0, max, 0.0, 1.0);
+
+    //println!("{:#?}", result);
+    return result;
+} 
+
+fn map(x: f32, in_min: f32, in_max: f32, out_min: f32, out_max: f32) -> f32 {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 fn read(filename: &String, line_num: i32) -> ([[f32; 8]; 8], bool) {
@@ -109,8 +139,8 @@ fn read(filename: &String, line_num: i32) -> ([[f32; 8]; 8], bool) {
     return (result, false);
 }
 
-fn getRange(filename: &String) -> (f32, f32) {
-    let (mut s, mut l) = (10000.0, 0.0);
+fn getMaxValue(filename: &String) -> f32 {
+    let mut r = 0.0;
 
     let mut line = 0;
     while line<99999 { 
@@ -120,18 +150,18 @@ fn getRange(filename: &String) -> (f32, f32) {
         }
         for y in 0..8 {
             for x in 0..8 {
-                if values[y][x] > l {
-                    l = values[y][x];
-                }
-                if values[y][x] < s {
-                    s = values[y][x];
+                if values[y][x] >  r {
+                    r = values[y][x];
+                    if r > 1200.0 {
+                        r = 1200.0;
+                    }
                 }
             }
         }
         line += 1;
     }
 
-    return (s, l);
+    return r;
 }
 
 fn main() {
@@ -158,13 +188,17 @@ fn main() {
         values: [[0.0; 8]; 8],
         last_updated: 0.0,
         line: 0,
+        max_value: getMaxValue(filename),
     };
 
     let mut events = Events::new(EventSettings::new());
 
     let mut vals = [[0.0; 8]; 8];
 
-    println!("{:#?}", getRange(filename));
+    if app.max_value==0.0 {
+        println!("Error could not range document");
+        return;
+    }
 
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
@@ -175,6 +209,6 @@ fn main() {
             app.update(&args, filename, &mut vals);
         }
     }
-
+    println!("max {}", app.max_value);
     println!("Ran {} frames", app.line);
 }
